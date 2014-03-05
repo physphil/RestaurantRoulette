143bebab9db404ac61a22433c83477ea4ea5a9f9
pshadlyn <physphil@gmail.com>
2014-03-05 16:48:26 -0500

    added user rating parameters to resource files

--- Restaurant Roulette/src/main/java/com/physphil/android/restaurantroulette/util/Constants.java
+++ Restaurant Roulette/src/main/java/com/physphil/android/restaurantroulette/util/Constants.java
@@ -9,8 +9,4 @@ public class Constants {
     public static final String FONT_ROBOTO_LIGHT = "roboto_light.ttf";
     public static final String FONT_DEFAULT = FONT_ROBOTO_LIGHT;
 
-    public static final int USER_RATING_MAX = 5;
-    public static final int USER_RATING_STEP_SIZE = 1;
-    public static final int PRICE_LEVEL_MAX = 4;
-    public static final int PRICE_LEVEL_STEP_SIZE = 1;
 }
