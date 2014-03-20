4e011c7a0b75693d3aa2668fd40820a2a9dd9822
pshadlyn <physphil@gmail.com>
2014-03-20 15:00:13 -0400

    added license info

--- Restaurant Roulette/src/main/java/com/physphil/android/restaurantroulette/util/Util.java
+++ Restaurant Roulette/src/main/java/com/physphil/android/restaurantroulette/util/Util.java
@@ -1,3 +1,24 @@
+/**
+ * Restaurant Roulette for Android
+ * Copyright (C) 2014  Phil Shadlyn
+ *
+ * Restaurant Roulette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2014 Phil Shadlyn - physphil@gmail.com
+ * @license GNU General Public License - https://www.gnu.org/licenses/gpl.html
+ */
+
 package com.physphil.android.restaurantroulette.util;
 
 import android.content.ActivityNotFoundException;
